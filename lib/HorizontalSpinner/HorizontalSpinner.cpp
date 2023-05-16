4eb99360fa5bfa4eacf48348208dc21b2ce2eec2
Jonathan Mi <jonathan.mi6@gmail.com>
2023-05-16 00:37:01 -0700

    Horizontal Spinner Framework

---
+++ lib/HorizontalSpinner/HorizontalSpinner.cpp
@@ -0,0 +1,17 @@
+#include "HorizontalSpinner.h"
+
+Pinball::HSpin::HorizontalSpinner::HorizontalSpinner(int motorPin)
+{
+    this->motorPin = motorPin;
+
+}
+
+void Pinball::HSpin::HorizontalSpinner::init()
+{
+
+}
+
+void Pinball::HSpin::HorizontalSpinner::update(unsigned long currTime)
+{
+
+}
---
+++ lib/HorizontalSpinner/HorizontalSpinner.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Arduino.h"
+#include "ScoringElement.h"
+#include "constants.h"
+
+namespace Pinball::HSpin
+{
+namespace Constants
+{
+    const int SPIN_SPEED = 255;
+}
+
+class HorizontalSpinner : public Pinball::ScoringElement
+{
+    private:
+        int motorPin;
+
+    public:
+        HorizontalSpinner(int motorPin);
+        void init();
+        void update(unsigned long currTime);
+};
+
+    
+}
+
+
