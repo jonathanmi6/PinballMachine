b7622c017bad38a626d5d69f4c5309d9167934d0
Jonathan Mi <jonathan.mi6@gmail.com>
2023-05-27 21:14:09 -0700

    fixed db logic

--- lib/DigitalDebounce/DigitalDebounce.cpp
+++ lib/DigitalDebounce/DigitalDebounce.cpp
@@ -27,24 +27,24 @@ void DigitalDebounce::init()
     setScore(0);
 }
 
-bool DigitalDebounce::update(unsigned long currTime)
+bool DigitalDebounce::update(unsigned long currTime) //TODO: after flag is set high, it doesn't get cleared if input was just a bounce
 {
     if(getInputRaw() && !triggedFlag) //sensor goes high, set flag to monitor
     {
         triggedFlag = true;
         trigTime = currTime;
     }
-    else if(getInputRaw() && triggedFlag && !waitForRelease && currTime > trigTime + dbTime) 
+    else if(getInputRaw() && triggedFlag && !waitForRelease && currTime > trigTime + dbTime) //check if debounce time has elapsed and sensor still trigged
     {
         waitForRelease = true;
         addScore(); //need to only add score ONCE
         return true;
     }
-    else if(getInputRaw() && triggedFlag && waitForRelease ) //so that update continues to return true while sensor is triggered
+    else if(getInputRaw() && triggedFlag && waitForRelease) //continue to return true while sensor is pressed
     {
         return true;
     }
-    else if(!getInputRaw() && triggedFlag && waitForRelease )
+    else if(!getInputRaw() && triggedFlag) //when released (after being pressed) reset flags, does not matter if db was reached or not
     {
         //sensor is released
         triggedFlag = false;
