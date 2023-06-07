3fa92a9de6ba11b68c4c06536db69e44749128df
Jonathan Mi <jonathan.mi6@gmail.com>
2023-06-06 23:07:55 -0700

    updated pop bumper constants

--- lib/PopBumper/PopBumper.h
+++ lib/PopBumper/PopBumper.h
@@ -7,10 +7,10 @@ namespace Pinball::PopBump
 namespace Constants
 {
     const unsigned long DELAY_TIME = 1; //min is 1ms
-    const unsigned long POP_TIME = 200;
+    const unsigned long POP_TIME = 300;
     const unsigned long HOLD_TIME = 1;
     const int POP_PERCENT = 255;
-    const int HOLD_PERCENT = 150;
+    const int HOLD_PERCENT = 100;
     const int OFF_PERCENT = 0;
 }
 class PopBumper: public Pinball::ScoringElement
